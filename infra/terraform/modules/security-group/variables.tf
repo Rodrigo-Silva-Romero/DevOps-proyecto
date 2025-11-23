7d826a6bf8437f26fc016738ed4aca87d098a708
Rodrigo Silva <153576329+Rodrigo-Silva-Romero@users.noreply.github.com>
2025-11-22 21:36:54 -0300

    Update variables.tf

--- infra/terraform/modules/security-group/variables.tf
+++ infra/terraform/modules/security-group/variables.tf
@@ -2,3 +2,7 @@ variable "vpc_id" { type = string }
 variable "vpc_cidr" { type = string }
 variable "environment" { type = string }
 variable "api_gateway_port" { type = number }
+variable "alb_sg_id" {
+  description = "Security Group ID del ALB para permitir tráfico hacia ECS"
+  type        = string
+}
