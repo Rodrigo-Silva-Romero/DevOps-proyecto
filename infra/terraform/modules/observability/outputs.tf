bae65460d4d3c36ba0716548e0fe46e69ab52575
Rodrigo Silva <153576329+Rodrigo-Silva-Romero@users.noreply.github.com>
2025-11-29 14:07:41 -0300

    Update outputs.tf

--- infra/terraform/modules/observability/outputs.tf
+++ infra/terraform/modules/observability/outputs.tf
@@ -1,14 +1,14 @@
 output "dashboard_name" {
   value       = aws_cloudwatch_dashboard.ecs_dashboard.dashboard_name
-  description = "Nombre del CloudWatch dashboard para ECS"
+  description = "Nombre del CloudWatch dashboard ECS"
 }
 
-output "ecs_cpu_high_alarm_arn" {
-  value       = aws_cloudwatch_metric_alarm.ecs_cpu_high.arn
-  description = "ARN de la alarma de CPU alta ECS"
+output "ecs_no_running_alarm_arn" {
+  value       = aws_cloudwatch_metric_alarm.ecs_no_running.arn
+  description = "ARN of ECS no running tasks alarm"
 }
 
-output "ecs_mem_high_alarm_arn" {
-  value       = aws_cloudwatch_metric_alarm.ecs_mem_high.arn
-  description = "ARN de la alarma de Memory alta ECS"
+output "ecs_pending_high_alarm_arn" {
+  value       = aws_cloudwatch_metric_alarm.ecs_pending_high.arn
+  description = "ARN of ECS pending high tasks alarm"
 }
