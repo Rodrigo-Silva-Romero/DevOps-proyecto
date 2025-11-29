aba557447e7cc7856f242bccbd0a89640b0fa3e4
Rodrigo Silva <153576329+Rodrigo-Silva-Romero@users.noreply.github.com>
2025-11-29 13:35:22 -0300

    Update outputs.tf

--- infra/terraform/modules/observability/outputs.tf
+++ infra/terraform/modules/observability/outputs.tf
@@ -1,14 +1,14 @@
 output "dashboard_name" {
-  value       = aws_cloudwatch_dashboard.vpc_dashboard.dashboard_name
-  description = "Nombre del CloudWatch dashboard"
-}
-output "network_in_zero_alarm_arn" {
-  value       = aws_cloudwatch_metric_alarm.network_in_zero.arn
-  description = "ARN of network-in-zero CloudWatch alarm"
+  value       = aws_cloudwatch_dashboard.ecs_dashboard.dashboard_name
+  description = "Nombre del CloudWatch dashboard para ECS"
 }
 
-output "network_out_high_alarm_arn" {
-  value       = aws_cloudwatch_metric_alarm.network_out_high.arn
-  description = "ARN of network-out-high CloudWatch alarm"
+output "ecs_cpu_high_alarm_arn" {
+  value       = aws_cloudwatch_metric_alarm.ecs_cpu_high.arn
+  description = "ARN de la alarma de CPU alta ECS"
 }
 
+output "ecs_mem_high_alarm_arn" {
+  value       = aws_cloudwatch_metric_alarm.ecs_mem_high.arn
+  description = "ARN de la alarma de Memory alta ECS"
+}
