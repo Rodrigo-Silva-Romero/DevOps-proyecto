8b06eec320df0219401ad403472646f3895af1ab
Rodrigo Silva <153576329+Rodrigo-Silva-Romero@users.noreply.github.com>
2025-11-29 13:41:23 -0300

    Update variables.tf

--- infra/terraform/modules/observability/variables.tf
+++ infra/terraform/modules/observability/variables.tf
@@ -1,12 +1,3 @@
-variable "vpc_id" {
-  description = "VPC ID"
-  type        = string
-}
-
-variable "subnet_ids" {
-  description = "List of subnet IDs"
-  type        = list(string)
-}
 
 variable "environment" {
   description = "Environment name"
