bca29d61f1370616d182b003b3eaeab293a32d38
Rodrigo Silva <153576329+Rodrigo-Silva-Romero@users.noreply.github.com>
2025-11-30 13:07:28 -0300

    Update outputs.tf

--- infra/infra-storage/outputs.tf
+++ infra/infra-storage/outputs.tf
@@ -1,8 +1,22 @@
+# Exponer el mapa completo de URLs
 output "ecr_urls" {
-  value = module.ecr.this[*].repository_url
+  value = module.ecr.ecr_urls
 }
 
+# Si querés exponer cada URL individual (para el pipeline)
+output "ecr_api_url" {
+  value = module.ecr.ecr_urls["apigateway"]
+}
+
+output "ecr_product_url" {
+  value = module.ecr.ecr_urls["product"]
+}
+
+output "ecr_inventory_url" {
+  value = module.ecr.ecr_urls["inventory"]
+}
+
+# Bucket S3
 output "s3_bucket_name" {
   value = module.s3.bucket_name
 }
-
